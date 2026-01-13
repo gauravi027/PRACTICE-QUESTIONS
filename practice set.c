6cf4df40ac252ecedc8de73d1a220a13483114af
gauravi027 <gauraviingle5@gmail.com>
2026-01-13 21:19:41 +0530

    Add C programs for various conditional checks
    Added multiple C programs to check voting eligibility, even/odd numbers, positivity, leap years, and if-else statement behavior.

---
+++ practice set.c
@@ -0,0 +1,80 @@
+-----------------------------------Question 1 -------------------------------------------------
+//code to check weather candidate is eligible for voting or not//
+#include <stdio.h>
+
+void main(){
+int age = 19;
+if (age > 18)
+{
+	printf("Eligible for vote");
+	
+}
+else
+{
+	printf("Not Eligible for voting process");
+}
+}
+--------------------------------Question 2 --------------------------------------------------
+//code to check weather number is even or odd//
+#include <stdio.h>
+
+void main(){
+	int no = 30;
+	if(no % 2 == 0)
+	{
+		printf("It is an even number");
+		
+	}
+	else
+	{
+		printf("It is an odd number");
+	}
+}
+-----------------------------------------Question 3 ----------------------------------------------------
+//code to check num is positive or negative//
+#include <stdio.h>
+
+void main(){
+	int no = 6;
+	if (no > 0 )
+	{
+		printf("It is an positive number");
+		
+	}
+	else
+	{
+		printf("It is an negative number");
+	}
+}
+---------------------------------------Question 4 ---------------------------------------------------
+//c prog code to check that year is leap //
+#include <stdio.h>
+
+void main(){
+	int year = 2000 ;
+	if(year%4==0)
+	{
+		printf("Year is leap");
+		
+	}
+	else
+	{
+		printf("Year is not leap");
+		
+	}
+}
+--------------------------------------Question 5 --------------------------------------------------
+//every nonzero number returns postive answer of if-else statement but if the number is 0 it will print else statement//
+#include <stdio.h>
+
+void main(){
+	if (-10)
+	{
+		printf("hello");
+	}
+	else
+	{
+		printf("bye");
+	}
+}
+----------------------------------------------------------------------------------------------------
